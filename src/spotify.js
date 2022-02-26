3d8e641e2cd47e0063bd79b654721a29cf77e8a2
vamsichamarthi1147 <86275315+chamarthivamsidev@users.noreply.github.com>
2022-02-26 14:51:20 +0530

    Added

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-// const redirectUri = "https://spotify-clone8.netlify.app/";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://spotify-clone-8.netlify.app/";
+// const redirectUri = "http://localhost:3000/";
 const clientId = "e2e41c8d6bb1493aafdf90e15fb98a8a";
 //230be2f46909426b8b80cac36446b52a
 
